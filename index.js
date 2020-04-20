bc09c75a04529807f03867e6d75cc6e784ea1d17
DESKTOP-DELTEBT\KHIN YADANA PHYO <khinyadanaphyo@u.nus.edu>
2020-04-20 18:05:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,3 +44,53 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      var senderID=webhook_event.sender.id;
+      console.log('senderID',senderID);
+      if(webhook_event.postback){
+        var userInput=webhook_event.postback.payload;
+    }
+    if (webhook_event.message) {if (webhook_event.message.text) {
+      var userInput=webhook_event.message.text;
+    }
+  if (webhook_event.message.attachments){
+    var userMedia=webhook_event.message.attachments.payload.url;
+
+
+    	if (userInput == 'Hi'){
+          let welcomeMessage = {
+            "recipient":{
+              "id":senderID
+            },
+            "message":{
+              "text":"Hello, Welcome to Curious Wheel!"
+            }
+          }};
+
+  }}
+   
+  });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+     } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
