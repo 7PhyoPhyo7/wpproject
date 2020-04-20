e3f76684d445c2b1fa2ccb890171b5489fc5eb16
DESKTOP-DELTEBT\KHIN YADANA PHYO <khinyadanaphyo@u.nus.edu>
2020-04-20 17:25:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,129 +1,15 @@
 'use strict';
+const
+PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
+  express = require('express'),
+  requestify = require('requestify'),
+  body_parser = require('body-parser'),
+  har = require('har-validator'),
+  admin= require('firebase-admin'),
+  app = express().use(body_parser.json()); // creates express http server
 
-// Imports dependencies and set up http server
-	const
-	requestify=require('requestify'),
-	express = require('express'),
-	bodyParser = require('body-parser'),
-	PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN,
-	app = express().use(bodyParser.json()); // creates express http server 
-	const sendmessageurl='https://graph.facebook.com/v6.0/me/messages?access_token='+PAGE_ACCESS_TOKEN
-
-	requestify.post('https://graph.facebook.com/v6.0/me/messenger_profile?access_token='+PAGE_ACCESS_TOKEN,
-		{"get_started":{"payload":"Hi"},
-		"persistent_menu":[
-			{
-				"locale":"default",
-				"composer_input_disabled":false,
-				"call_to_actions":[
-				{
-					"type":"postback",
-					"title":"Homehello",
-					"payload":"Hi"
-
-				},
-				{
-					"type":"web_url",
-					"title":"Visit Page",
-					"url":"https://mym-acavxb.firebaseapp.com/index.html",
-					"webview_height_ratio":"tall"
-
-				}
-			]
-	
-		}
-	],
-  
-  "greeting": [
-    {
-      "locale":"default",
-      "text":"Hello {{user_first_name}}! \nWe provide service!!" 
-    }
-  ]
-
-}).then(function(success) {
-	console.log('persistent_menu.success');
-	// body...
-})
-
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+  app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 app.get('/', (req, res)=>{
-	res.send("Hello vro!");
+	res.send("Hello Oppa!");
 })
-
-
-
-// Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
-
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
-   
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-   	   res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
-});
-
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-      var senderID=webhook_event.sender.id;
-      console.log('senderID',senderID);
-      if(webhook_event.postback){
-      	var userInput=webhook_event.postback.payload;
-    }
-    if (webhook_event.message) {if (webhook_event.message.text) {
-    	var userInput=webhook_event.message.text;
-    }
-	if (webhook_event.message.attachments){
-		var userMedia=webhook_event.message.attachments.payload.url;
-
-	}}
-	  
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-     } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
-
-
-
-
-
-
