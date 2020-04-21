665795cbf364ad638f9eb71f617adef9670211a6
DESKTOP-DELTEBT\KHIN YADANA PHYO <khinyadanaphyo@u.nus.edu>
2020-04-22 00:08:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function removePersistentMenu(res){
   function (error, response, body) {
       if (!error && response.statusCode == 200) {          
           res.send(body);
-      } else {           
+      } else { 								          
           res.send(body);
       }
   });
